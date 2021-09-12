ceeba797040471d32a6ffc4091b5faa89771ca43
raneyj <raney.jack99@gmail.com>
2021-09-11 20:24:45 -0500

    Minor changes to ui

--- src/components/ViewCollectionComponent.tsx
+++ src/components/ViewCollectionComponent.tsx
@@ -1,8 +1,11 @@
 import { Principal } from "../dtos/principal";
 import { Collections } from "../dtos/collection";
+import { Question } from "../dtos/question";
 import { Redirect , Link } from "react-router-dom";
 import {useState, useEffect} from "react";
 import {getCollection} from "../remote/collection-service";
+import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
 
 
 
@@ -46,9 +49,44 @@ function ViewCollectionComponent(props: IViewProps) {
 
         } catch (e: any) {
             setErrorMessage(e.message); 
+        }   
+    }
+
+    function edit(question : Question | undefined) {
+        /*
+        if(!collection) {
+            return;
         }
 
-            
+        setShowEdit(true);
+        setCurrentCollection(collection);
+        return undefined;
+        */
+    }
+
+    function remove(question : Question | undefined) {
+        /*
+        if(!collection) {
+            return;
+        }
+
+        setShowDelete(true);
+        setCurrentCollection(collection);
+
+        return undefined;
+        */
+    }
+
+    function getComponent() {
+        /*
+        if(showDelete) {
+            return <DeleteCollectionModal current_user={props.currentUser} collection={currentCollection} show={showDelete} setShow={setShowDelete} updateUI={removeUI}/>;
+        } else if(showCreate) {
+            return <CreateCollectionModal current_user={props.currentUser} show={showCreate} setShow={setShowCreate} updateUI={createUI}/>;
+        } else if(showEdit){
+            return <EditCollectionModal current_user={props.currentUser}  collection={currentCollection} show={showEdit} setShow={setShowEdit} updateUI={removeUI}/>;
+        }
+        */
     }
 
 
@@ -59,6 +97,30 @@ function ViewCollectionComponent(props: IViewProps) {
             <h1>Manage "{props.collection?.title}" by {props.currentUser.username}</h1>
             <h6>Category: {props.collection?.category}</h6>
             <h6>Description: {props.collection?.description}</h6>
+            <Table  striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                          <td>Question</td>
+                          <td>Answer</td>
+                          <td>Manage</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {props.collection?.questionList?.map((Q : Question | undefined, i) =>{
+                           
+                        return  <tr key={i}>
+                                    <td>{Q?.question} </td>
+                                    <td>{Q?.answer}</td>
+                                    
+                                    <td>
+                                    <Button variant="secondary" onClick={() => edit(Q)}>Edit</Button> {  }
+                                    <Button variant="secondary" onClick={() => remove(Q)}>Delete</Button> {  }
+                                    </td>
+                                </tr> 
+                    })}
+                    {getComponent()}
+                    </tbody>
+                </Table>
         </>
         :
         <Redirect to="/login"/>
