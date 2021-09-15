61bf2ef701a72abbd1f3d3c99f0b891d279ee6a9
raneyj <raney.jack99@gmail.com>
2021-09-14 22:08:53 -0500

    Fixed env.js routing

--- src/util/env.js
+++ src/util/env.js
@@ -3,6 +3,6 @@ const APP_ROOT = document.getElementById('app-root');
 export default {
     rootDiv: APP_ROOT,
     //apiUrl: 'http://Registrarapp-env.eba-pxifbtnp.us-east-2.elasticbeanstalk.com'
-    //apiUrl: 'http://jash-env.eba-8z8mrahn.us-east-2.elasticbeanstalk.com'
-    apiUrl: 'http://localhost:5000'
+    apiUrl: 'http://jash-env.eba-8z8mrahn.us-east-2.elasticbeanstalk.com'
+    //apiUrl: 'http://localhost:5000'
 }
