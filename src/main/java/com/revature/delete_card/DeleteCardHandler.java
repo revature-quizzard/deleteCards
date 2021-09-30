66b604a763c33f2da1543c171a551727a96e85f0
AlfonsoHolmesData <alholmes@Email.peace.edu>
2021-09-30 14:21:27 -0400

    added check methods to the updateUserCollections method
    Signed-off-by: AlfonsoHolmesData <alholmes@Email.peace.edu>

--- src/main/java/com/revature/delete_card/DeleteCardHandler.java
+++ src/main/java/com/revature/delete_card/DeleteCardHandler.java
@@ -39,25 +39,29 @@ public DeleteCardHandler(SetRepository setRepo, UserRepository userRepo) {
 public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
 
         LambdaLogger logger = context.getLogger();
-        logger.log("RECEIVED EVENT: " + requestEvent);
+
 
         APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
+
         //getting id out of request body
         String target_id = requestEvent.getPathParameters().get("id");
         String card_id = requestEvent.getPathParameters().get("card_id");
-        // attempting to delete target set based on id
+
         try{
         // deleting card from Sets table
         Set updated_set = setRepo.deleteCardBySetId(target_id , card_id);
         // using updated set from table to update the rest of users
         userRepo.updateUserCollections(updated_set);
         responseEvent.setBody(mapper.toJson(updated_set));
+        responseEvent.setStatusCode(200);
         }catch (ResourceNotFoundException rnfe) {
         responseEvent.setStatusCode(404);
+        }catch (Exception e) {
+        logger.log("RECEIVED ERROR MESSAGE: " + e.getMessage() + "\n");
+        responseEvent.setStatusCode(500);
         }
 
-
-        responseEvent.setStatusCode(200);
+        logger.log("RECEIVED EVENT: " + responseEvent.getBody());
         return responseEvent;
         }
 }
--- src/main/java/com/revature/delete_card/Repositories/UserRepository.java
+++ src/main/java/com/revature/delete_card/Repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.revature.delete_card.Repositories;
 
 import com.revature.delete_card.Documents.Set;
 import com.revature.delete_card.Documents.User;
+import com.revature.delete_card.Execptions.InvalideRequestException;
 import com.revature.delete_card.Execptions.ResourceNotFoundException;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
@@ -29,7 +30,9 @@ public class UserRepository {
      * Gets all users from the Users table
      * @Authors Alfonso Holmes
      */
-    public List<User> getAllUsers(){ return userTable.scan().items().stream().collect(Collectors.toList()); }
+    public List<User> getAllUsers(){
+        return userTable.scan().items().stream().collect(Collectors.toList());
+    }
 
     /**
      * Gets a given user from the Users table by username
@@ -40,7 +43,7 @@ public class UserRepository {
         AttributeValue val = AttributeValue.builder().s(name).build();
         Expression filter = Expression.builder().expression("#a = :b").putExpressionName("#a", "username") .putExpressionValue(":b", val).build();
         ScanEnhancedRequest request = ScanEnhancedRequest.builder().filterExpression(filter).build();
-        User user = userTable.scan(request).stream().findFirst().orElseThrow(ResourceNotFoundException::new).items().get(0);
+        User user = userTable.scan(request).items().stream().findFirst().orElseThrow(ResourceNotFoundException::new);
         System.out.println("USER WITH ID: " + user);
         return user;
     }
@@ -54,13 +57,30 @@ public class UserRepository {
     public void updateUserCollections(Set updated_set) {
         //get rid of set from author's created_sets and all users favorite_sets
         List<User> users = getAllUsers();
+
+        // if there are no users throw exception
+        if(users.size() == 0) { throw new ResourceNotFoundException(); }
+        // if there is only one user and they have not created or favorite sets or either set has no cards throw exception
+        if(users.size() == 1){
+            if(users.get(0).getFavorite_sets().size() == 0 || users.get(0).getCreated_sets().size() == 0) { throw new ResourceNotFoundException(); }
+            if(users.get(0).getFavorite_sets().get(0).getCards().size() == 0 || users.get(0).getCreated_sets().get(0).getCards().size() == 0) { throw new InvalideRequestException(""); }
+        }
+
         for(User u : users) {
+
+            // if users have no created or favorite sets skip them or either set has no cards (next 11 lines)
+            ///////////////////////////////////////////////////////////////////////////////////////
+            if(u.getFavorite_sets().size() == 0 || u.getCreated_sets().size() == 0) { continue; }
+            if(invalidateCardCheck(u.getCreated_sets())) { continue; }
+            if(invalidateCardCheck( u.getFavorite_sets())) { continue; }
+            /////////////////////////////////////////////////////////////////////////////////////////
 
             /*
                  * iterating thought users created sets
                  * searching for the created user set that matched the updated_set id
                  * replacing target sets card array with the updated one
             */
+
             for (User.UserSetDoc set : u.getCreated_sets()) {
                 if (set.getId().equals(updated_set.getId())) {
                     set.setCards(updated_set.getCards());
@@ -80,5 +100,21 @@ public class UserRepository {
 
             userTable.putItem(u);
         }
+    }
+
+    /**
+     * searches Sets list for an empty card list and returns true if empty list is found
+     * @param in_list the set in witch to check (must be of type User.UserSetDoc)
+     * @return
+     * @Author Alfonso
+     */
+    boolean invalidateCardCheck(List<User.UserSetDoc> in_list)
+    {
+        for (User.UserSetDoc set : in_list) {
+            if (set.getCards().size() == 0 ) {
+                return true;
+            }
+        }
+        return false;
     }
 }
