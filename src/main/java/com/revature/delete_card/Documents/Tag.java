a7eee2aebe97f06a657246f6e36a75e1a65ff839
AlfonsoHolmesData <alholmes@Email.peace.edu>
2021-09-30 11:06:37 -0400

    delete card branch init
    Signed-off-by: AlfonsoHolmesData <alholmes@Email.peace.edu>

---
+++ src/main/java/com/revature/delete_card/Documents/Tag.java
@@ -0,0 +1,40 @@
+package com.revature.delete_card.Documents;
+
+
+import lombok.Data;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+@Data
+@DynamoDbBean
+public class Tag {
+
+    private String name;
+    private String color;
+
+    @DynamoDbPartitionKey
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Tag(String name){
+        this.name = name;
+    }
+
+    public Tag() {
+        super();
+    }
+
+}
