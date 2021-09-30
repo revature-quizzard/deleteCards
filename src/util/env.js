fd13455bdb94d7bb35866f90de2fb60b0d2a809d
AlfonsoHolmesData <71735158+AlfonsoHolmesData@users.noreply.github.com>
2021-09-30 10:26:14 -0400

    Delete src directory

--- src/util/env.js
+++
@@ -1,8 +0,0 @@
-const APP_ROOT = document.getElementById('app-root');
-
-export default {
-    rootDiv: APP_ROOT,
-    //apiUrl: 'http://Registrarapp-env.eba-pxifbtnp.us-east-2.elasticbeanstalk.com'
-    apiUrl: 'http://jash-env.eba-8z8mrahn.us-east-2.elasticbeanstalk.com'
-    //apiUrl: 'http://localhost:5000'
-}
